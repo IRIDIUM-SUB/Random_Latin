af2908e507aa2b01362e3ffef1a20b17f5f2001d
potassium <novapotassium@gmail.com>
2022-04-06 13:20:19 +0800

    Initialize commit

---
+++ random_latin.py
@@ -0,0 +1,11 @@
+from crypt import methods
+import json
+from flask import Flask, abort, request, jsonify
+import time
+app=Flask(__name__)
+@app.route('/random_latin/',methods=['GET'])
+def genlatin()->json:
+    pass
+if __name__ == "__main__":
+    # 将host设置为0.0.0.0，则外网用户也可以访问到这个服务
+    app.run(host="127.0.0.1", port=80, debug=True)
